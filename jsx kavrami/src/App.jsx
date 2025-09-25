5a029b905f3e6860ec8669236046a94413ef4461
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-25 21:08:22 +0300

    Update App.jsx

--- jsx kavrami/src/App.jsx
+++ jsx kavrami/src/App.jsx
@@ -1,19 +1,16 @@
 import "./App.css";
 
 function App() {
-  let vize1 = 48;
-  let vize2 = 59;
-  let vize3 = 71;
-  let sonuc = (vize1 + vize2 + vize3) / 3.0;
-  sonuc = Math.round(sonuc);
+  
+  let isimler = ["ali", "ayşe", "fatma"];
   return (
     <div>
-      {sonuc > 50 ? (
-        <p>Sınavdan Geçtiniz: {sonuc}</p>
-      ) : (
-        <p>Sınavdan Kaldınız: {sonuc}</p>
-      )}
+      {isimler.map((isim) =>(
+        <div>{isim}</div>
+      
+      ))
+      }
     </div>
-  );
+  )
 }
 export default App;
