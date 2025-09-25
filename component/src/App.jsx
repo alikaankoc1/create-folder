88cdfa38e29bb2c90d0cb52204de0b3c793c5c86
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-25 21:42:12 +0300

    update login

--- component/src/App.jsx
+++ component/src/App.jsx
@@ -5,6 +5,8 @@ import Login from './components/Login'
 function App() {
   return (
     <div>
+ <Login/> 
+ <br /> <hr /> <hr />
  <Login/>
     </div>
   )
--- component/src/components/Login.jsx
+++ component/src/components/Login.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <div>
+            <p>Adınız Soyadınınız:</p>
+            <input type="text" />
+        </div>
+        <div>
+            <p>E-Mail:</p>
+            <input type="email" />
+        </div>
+        <div>
+            <p>Şifreniz:</p>
+            <input type="password" />
+        </div>
+    </div>
   )
 }
 
