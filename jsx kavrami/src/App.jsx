aff334ac982c363ba7f7c754855bfbde92f1eba1
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-25 20:42:49 +0300

    Update App.jsx

--- jsx kavrami/src/App.jsx
+++ jsx kavrami/src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 
 function App() {
   let a = 22;
+  const firstName = "ali kaan";
   return (
     <div>
       <p>a nın değer {a}</p>
+      <p>isim {firstName}</p>
     </div>
   );
 }
