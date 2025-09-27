a0dd0e9ab1ddd4a49c84a7cc172a5f02510d7861
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-27 17:56:57 +0300

    props yapısı

--- props structure/src/App.jsx
+++ props structure/src/App.jsx
@@ -2,16 +2,10 @@ import React from 'react'
 import Product from './Product'
 
 function App() {
-  const dataBase = "Buzdolabı";
   return (
-    <div>
-    <Product productName = "Ayakkabı" price = {3200}/> 
-    <hr />
-    <Product productName ="Pantolon" price ={1100}/>
-    <hr />
-    <Product productName = {dataBase} price ={56000}/>
-    </div>
-  
+   <div>
+    <Product productName = "ayakkabı" price = {5000}/>
+   </div>
   )
 }
 
--- props structure/src/Product.jsx
+++ props structure/src/Product.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 function Product(props) {
   return (
     <div>
-      <div>ÜRÜN BİLGİLERİ</div>
-      <div>
-        <div>{props.productName}</div>
-        <div> {props.price} </div>
-      </div>
+        <div>Ürün Bilgileri</div>
+        <div>
+            <div>Ürün Adı: {props.productName}</div>
+            <div>Ürün Fiyatı: {props.price}</div>
+        </div>
     </div>
+    
   )
 }
 
