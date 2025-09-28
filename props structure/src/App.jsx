135d77f21ddb78bd13b63a7e4f6ff0a610e829fc
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-09-28 18:15:05 +0300

    Update App.jsx

--- props structure/src/App.jsx
+++ props structure/src/App.jsx
@@ -4,7 +4,8 @@ import Product from './Product'
 function App() {
   return (
     <div>
-      <Product status = {"güneşli"}/>
+      <Product status = {"güneşli"}
+      />
       <Product status = {"yağmurlu"}/>
     </div>
   )
